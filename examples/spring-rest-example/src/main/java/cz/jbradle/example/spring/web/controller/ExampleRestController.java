8adf9001b12196b87195860d5116d638421419be
Jiri Bradle <bradle.jiri@gmail.com>
2016-03-29 16:04:02 +0200

    UPDATE# Spring rest example

--- examples/spring-rest-example/src/main/java/cz/jbradle/example/spring/web/controller/ExampleRestController.java
+++ examples/spring-rest-example/src/main/java/cz/jbradle/example/spring/web/controller/ExampleRestController.java
@@ -2,13 +2,14 @@ package cz.jbradle.example.spring.web.controller;
 
 import cz.jbradle.example.spring.web.model.ExampleEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest")
 public class ExampleRestController {
 
-	@RequestMapping("/example")
+	@RequestMapping(value = "/example", method = RequestMethod.GET)
 	public ExampleEntity example() {
 		return new ExampleEntity(1, "Example value");
 	}
